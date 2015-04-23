8c92fddd27be4a5aecd7e25d4626f44f1ea12477
Michael Cerezo <cerezo.michael.mc@gmail.com>
2015-04-23 11:14:24 -0700

    video 5

--- index.php
+++ index.php
@@ -1,12 +1,19 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>To-Do List 2.0</title>
+	<title>My To-Do List</title>
 	<link rel="stylesheet" type="text/css" href="css/main.css">
 </head>
 <body>
 	<div class="wrap">
-	
+		<div>
+			<ul>
+				<?php require ("includes/connect.php") ?>
+			</ul>		
+		</div>
+		<form class="add-new-task" autocomplete="off">
+			<input type="text" name="new-task" placeholder="Add new item..."/>
+		</form>
 	</div>
 </body>
 </html>
