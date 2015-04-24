6c054673985eeebcf4eb660dee74b87553eb943d
Michael Cerezo <cerezo.michael.mc@gmail.com>
2015-04-23 21:40:21 -0700

    video 16

--- index.php
+++ index.php
@@ -8,22 +8,21 @@
 	<div class="wrap">
 		<div class="task-list">
 			<ul>
-				<?php require ("includes/connect.php");
+				<?php require ("includes/connect.php")
 					$mysqli = new mysqli('localhost', 'root', 'root', 'todo');
 					$query = "SELECT * FROM tasks ORDER BY date ASC, time ASC";
 					if ($result = $mysqli->query($query)) {
-						$numrows = $result->num_rows;
+						$numrows = $result->num_rows:
 						if ($numrows>0){
 							while($row = $result->fetch_assoc()){
 								$task_id = $row['id'];
 								$task_name = $row["task"];
-								echo "<li>
-								<span>'.$task_name'
-								";
+								echo '<li>
+								<span>'.$task_id.'"class="delete-button" width="10px" src="images/close.svg"/>
+								</li>';
 							}
 						}
 					}
-					
 				 ?>
 			</ul>		
 		</div>	
