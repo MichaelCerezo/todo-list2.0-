0424e6112b8f9d5b792fb26e5ee7c0afde85c9e7
Michael Cerezo <cerezo.michael.mc@gmail.com>
2015-04-23 21:52:24 -0700

    video 18

--- index.php
+++ index.php
@@ -3,16 +3,19 @@
 <head>
 	<title>My To-Do List</title>
 	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<link rel="stylesheet" type="text/css" href="css/normalize.css">
+	<link rel="stylesheet" type="text/css" href="css/reset.css">
+
 </head>
 <body>
 	<div class="wrap">
 		<div class="task-list">
 			<ul>
-				<?php require ("includes/connect.php")
+				<?php require ("includes/connect.php");
 					$mysqli = new mysqli('localhost', 'root', 'root', 'todo');
 					$query = "SELECT * FROM tasks ORDER BY date ASC, time ASC";
 					if ($result = $mysqli->query($query)) {
-						$numrows = $result->num_rows:
+						$numrows = $result->num_rows;
 						if ($numrows>0){
 							while($row = $result->fetch_assoc()){
 								$task_id = $row['id'];
